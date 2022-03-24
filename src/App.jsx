95b62c4adef60824609acd9b7c16ab91072c982b
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-24 23:13:56 +0600

    Full project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Card from './Components/Card/Card';
-import Nav from './Components/Nav/Nav';
-import Modal from 'react-modal';
-import { CgCloseR } from "react-icons/cg"
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./Components/Card/Card";
+import Nav from "./Components/Nav/Nav";
+import Modal from "react-modal";
+import { CgCloseR } from "react-icons/cg";
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
   },
 };
 
@@ -26,35 +26,51 @@ function App() {
   function closeModal() {
     setIsOpen(false);
   }
-  const [guns, setGuns] = useState([])
-  const [cart, setCart] = useState([])
-    useEffect(() => {
-        fetch('data.json')
-        .then(res => res.json()).then(data => setGuns(data))
-    }, [])
+  const [guns, setGuns] = useState([]);
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setGuns(data));
+  }, []);
   const addToCart = (gun) => {
-    const newCart = [...cart, gun]
-    setCart(newCart)
-    }
-   
+    const newCart = [...cart, gun];
+    setCart(newCart);
+  };
+
   return (
     <div>
-      <Nav cart={cart}  openModal={openModal} />
+      <Nav cart={cart} openModal={openModal} />
       <div className="card-container">
-        {guns.map(gun => <Card addToCart={addToCart}  guns={gun}/>)}
+        {guns.map((gun) => (
+          <Card addToCart={addToCart} guns={gun} />
+        ))}
       </div>
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles}>
-        <button className='modal-close-button' onClick={closeModal}>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <button className="modal-close-button" onClick={closeModal}>
           <CgCloseR size={25} />
         </button>
-        {cart.length === 0 && (
-          <div className='cart-warning'>
+        {cart.length === 0 && ( // ? confused
+          <div className="cart-warning">
             <p> Cart is empty </p>
           </div>
         )}
+        <div className="modal-container">
         {cart.map((item) => (
-          <h1>Name: {item.name}</h1>
+          <div className="model">
+            <img width="100px" src={item.img} alt="" />
+            <div className="model-para">
+              <h2>{item.name}</h2>
+              <p><strong>Price:</strong> ${item.price}</p>
+              <p><strong>Bullet Type:</strong> {item.bullet}</p>
+            </div>
+          </div>
         ))}
+       </div>
       </Modal>
     </div>
   );
