1876ac1ec7fc0d193717cd4add516e711d3cec49
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-24 22:21:46 +0600

    Model is designed

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,30 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './Components/Card/Card';
 import Nav from './Components/Nav/Nav';
+import Modal from 'react-modal';
+import { CgCloseR } from "react-icons/cg"
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 function App() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   const [guns, setGuns] = useState([])
   const [cart, setCart] = useState([])
     useEffect(() => {
@@ -17,10 +39,23 @@ function App() {
    
   return (
     <div>
-      <Nav cart={cart}/>
+      <Nav cart={cart}  openModal={openModal} />
       <div className="card-container">
         {guns.map(gun => <Card addToCart={addToCart}  guns={gun}/>)}
       </div>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles}>
+        <button className='modal-close-button' onClick={closeModal}>
+          <CgCloseR size={25} />
+        </button>
+        {cart.length === 0 && (
+          <div className='cart-warning'>
+            <p> Cart is empty </p>
+          </div>
+        )}
+        {cart.map((item) => (
+          <h1>Name: {item.name}</h1>
+        ))}
+      </Modal>
     </div>
   );
 }
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import './Nav.css'
-import { BsCartFill } from 'react-icons/bs';
+import React from "react";
+import "./Nav.css";
+import { BsCartFill } from "react-icons/bs";
 
-const Nav = ({cart}) => {
-    return (
-        <nav className='navbar'>
-            <h1>PUBG Gun Store</h1>
-            <div className='cart-counter'>
-                <span>{cart.length }</span>
-            <BsCartFill  className='icon' color='#fff' />
-            </div>
-        </nav>
-    );
+
+
+
+const Nav = ({ cart, openModal }) => {
+
+  return (
+    <nav className="navbar">
+
+      <h1>PUBG Gun Store</h1>
+        <div onClick={openModal} className="cart-counter">
+          <span>{cart.length}</span>
+          <BsCartFill className="icon" color="#fff" />
+        </div>
+    </nav>
+  );
 };
 
 export default Nav;
